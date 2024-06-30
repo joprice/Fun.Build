e0b30391d7c461d94f38640bba6702da8b735691
Joseph Price <pricejosephd@gmail.com>
2024-06-30 14:09:47 -0400

    add sigterm call

--- Fun.Build/ProcessExtensions.fs
+++ Fun.Build/ProcessExtensions.fs
@@ -10,6 +10,69 @@ open System.Runtime.InteropServices
 open Spectre.Console
 open Fun.Build.Internal
 
+open System.Diagnostics
+open System
+open System.Runtime.InteropServices
+
+#if WINDOWS
+//    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)>]
+//    extern IntPtr private SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+//
+//    [<Literal>]
+//    let private WM_CLOSE = 0x10u;
+//    let private terminate handle =
+//        SendMessage(handle, WM_CLOSE, IntPtr.Zero, IntPtr.Zero) |> ignore
+let private terminate (p: Process) = if p.CloseMainWindow() then Ok() else Error ""
+#else
+open Microsoft.FSharp.NativeInterop
+
+[<DllImport("libc", SetLastError = true)>]
+extern int private kill(int pid, int signal)
+
+[<DllImport("libc", SetLastError = true)>]
+extern int private strerror_r(int errnum, char* buf, UInt64 buflen)
+
+let private getErrorMessage errno =
+    let buffer = NativePtr.stackalloc<char> 1024
+    let result = strerror_r (errno, buffer, 1024UL)
+
+    if result = 0 then
+        Marshal.PtrToStringAnsi(buffer |> NativePtr.toNativeInt)
+    else
+        $"errno %i{errno}"
+
+[<Literal>]
+let private SIGTERM = 15
+[<Literal>]
+let private ESRCH = 3
+let private terminate pid =
+    let code = kill (pid, SIGTERM)
+    let errno = Marshal.GetLastWin32Error()
+    if code = -1 && errno <> ESRCH then // ESRCH = process does not exist, assume it exited
+        getErrorMessage errno |> Error
+    else
+        Ok code
+#endif
+
+let termProcess (p: Process) =
+    if p.HasExited then
+        Ok()
+    else
+#if WINDOWS
+        match terminate p with
+#else
+        match terminate p.Id with
+#endif
+        | Ok _ -> Ok()
+        | Error msg ->
+            p.Kill()
+            Error $"Unable to gracefully stop %s{p.ProcessName}. Killed process instead. %s{msg}"
+
+let terminateProcess (p: Process) =
+    match termProcess p with
+    | Ok _ -> ()
+    | Error error -> printfn "Failed shutting down process %d" p.Id
+
 type Process with
 
     static member GetQualifiedFileName(cmd: string) =
@@ -68,11 +131,12 @@ type Process with
                 startInfo.StandardOutputEncoding <- Encoding.UTF8
                 startInfo.StandardErrorEncoding <- Encoding.UTF8
 
-            startInfo.CreateNoWindow <- true
-            startInfo.UseShellExecute <- true
+            //startInfo.CreateNoWindow <- true
+            //startInfo.UseShellExecute <- true
             //startInfo.UseShellExecute
             use result = Process.Start startInfo
-            result.EnableRaisingEvents <- true
+            printfn "id %d" result.Id
+            //result.EnableRaisingEvents <- true
             let standardOutputSb = StringBuilder()
 
             let handleDataReceived (ev: DataReceivedEventArgs) =
@@ -93,8 +157,9 @@ type Process with
 
                     AnsiConsole.WriteLine $"{commandLogString} is cancelled or timed out and the process will be killed."
 
-                    result.Kill()
-                    result.WaitForExit()
+                    // result.Kill()
+                    terminateProcess result
+                //result.WaitForExit()
                 )
 
             use _ =
@@ -102,14 +167,15 @@ type Process with
                 | Some ct ->
                     ct.Register(fun () ->
                         AnsiConsole.MarkupLine("[yellow]Command is cancelled by your token[/]")
-                        result.Kill()
-                        AnsiConsole.MarkupLine("[yellow]Waiting for exit[/]")
-                        while not result.HasExited && not result.Responding do
-                            AnsiConsole.MarkupLine("[yellow]not exited[/]")
-                            Thread.Sleep 1000
-                        // result.CancelOutputRead()
-                        // result.CancelErrorRead()
-                        result.WaitForExit()
+                        terminateProcess result
+                    //result.Kill()
+                    // AnsiConsole.MarkupLine("[yellow]Waiting for exit[/]")
+                    // while not result.HasExited && not result.Responding do
+                    //     AnsiConsole.MarkupLine("[yellow]not exited[/]")
+                    //     Thread.Sleep 1000
+                    // // result.CancelOutputRead()
+                    // // result.CancelErrorRead()
+                    // result.WaitForExit()
                     // if (result.HasExited) then
                     //     AnsiConsole.MarkupLine("[yellow]exited[/]")
                     // else
